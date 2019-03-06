800e772988447db0858355aee45efc74a95c922b
Daryl <daryl@briviant.com>
2019-03-06 05:49:29 -0800

    beginning portion of project

---
+++ new2_0.js
@@ -0,0 +1,4 @@
+var myNullVar;
+var myNullVar2;
+console.log(typeof myNullVar);
+console.log(typeof myNullVar2);
---
+++ new2_0.ts
@@ -0,0 +1,18 @@
+var myNullVar: null;
+
+var myNullVar2: undefined;
+
+
+console.log(typeof myNullVar);
+
+console.log(typeof myNullVar2);
+
+
+if (myNullVar2 === undefined) {
+
+    console.log("oh yeah nice typescript2.0 feature");
+
+
+}
+
+
