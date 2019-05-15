875d567c9324dd140d96919113f86d22fb9ff694
Daryl <daryl@briviant.com>
2019-05-15 01:23:47 -0700

    added new file to introduce some new features of typescript, added warrior interface and created new warrior object with it

---
+++ new2_1.ts
@@ -0,0 +1,28 @@
+interface Warrior {
+
+    name: string;
+
+    weapon: string;
+
+    strength: number;
+
+    agility: number;
+
+}
+
+{let hunora: Warrior = {     
+    
+                            name: 'Hunora',
+
+                            weapon: 'swords',
+
+                            strength: 4,
+
+                            agility: 5
+
+                       }   
+
+
+    console.log(hunora);
+
+}
